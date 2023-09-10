552f8805943c3dcbd0c930287bd8d1ab059628cf
Cassio S C <oissac22@hotmail.com>
2023-09-10 17:47:43 -0300

    start project

---
+++ src/main/java/com/crochepoint/crochepoint/CrochepointApplication.java
@@ -0,0 +1,13 @@
+package com.crochepoint.crochepoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochepointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrochepointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crochepoint/crochepoint/CrochepointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crochepoint.crochepoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochepointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
